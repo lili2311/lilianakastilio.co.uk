c083778952b0fd79783cc1186dd1c0bd1d3de55a
Liliana Kastilio <kastilio.liliana@gmail.com>
2014-01-16 22:49:00 +0000

    Added in Blog

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,80 +1,31 @@
 module.exports = function(grunt) {
 
-  grunt.loadNpmTasks('grunt-s3');
+  grunt.loadNpmTasks('grunt-aws-s3');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     s3settings: grunt.file.readJSON('s3settings.json'),
 
-    s3: {
+    aws_s3: {
       options: {
-        key: '<%= s3settings.key %>',
-        secret: '<%= s3settings.secret %>',
-        bucket: '<%= s3settings.bucket %>',
+        accessKeyId: '<%= s3settings.key %>', // Use the variables
+        secretAccessKey: '<%= s3settings.secret %>', // You can also use env variables
         region: '<%= s3settings.region %>',
-        access: 'public-read',
-        //debug: true,
-        headers: {
-          // Two Year cache policy (1000 * 60 * 60 * 24 * 730)
-          "Cache-Control": "max-age=630720000, public",
-          "Expires": new Date(Date.now() + 63072000000).toUTCString()
-        },
-        gzip: true
+        uploadConcurrency: 5, // 5 simultaneous uploads
+        downloadConcurrency: 5 // 5 simultaneous downloads
       },
       live: {
-        upload: [
-          {
-            src: '*.html',
-            dest: '/',
-          },
-          {
-            src: 'js/*.js',
-            dest: 'js/',
-          },
-          {
-            src: 'css/*.css',
-            dest: 'css/',
-          },
-          {
-            src: 'images/*.svg',
-            dest: 'images/',
-          },
-          {
-            src: 'stickyheader/*.html',
-            dest: 'stickyheader/',
-          },
-          {
-            src: 'stickyheader/*.js',
-            dest: 'stickyheader/',
-          },
-          {
-            src: 'stickyheader/*.css',
-            dest: 'stickyheader/',
-          },
-          {
-            src: 'meow/images/*.png',
-            dest: 'meow/images/',
-
-          },
-          {
-            src: 'meow/*.html',
-            dest: 'meow/',
-          },
-          {
-            src: 'meow/*.js',
-            dest: 'meow/',
-          },
-          {
-            src: 'meow/*.css',
-            dest: 'meow/',
-          }
-
+        options: {
+          bucket: '<%= s3settings.bucket %>',
+          differential: true // Only uploads the files that have changed
+        },
+        files: [
+          {expand: true, cwd: 'src/', src: ['**'], dest: ''},
         ]
       }
     }
-    
   });
 
-  grunt.registerTask('deploy', ['s3:live']);
+  grunt.registerTask('deploy', ['aws_s3:live']);
 
 };
